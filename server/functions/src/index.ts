64efa9eeebeb00d28db3fd6f2b3f85b08e0d4b6b
Maxim <max.mrtnv@gmail.com>
2020-03-02 19:21:08 +0400

    Configuration: Environment

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,5 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
-});
+
+export const helloWorld = functions
+  .region(config.CLOUD_FUNCTIONS_REGION)
+  .https.onRequest((request, response) => {
+    response.send('Hello from Cash Flow!');
+  });
