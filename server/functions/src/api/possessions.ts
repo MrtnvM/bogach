d3c4e40b0cdb56ddc8e043c8daa3ec7d5025fae0
Maxim <max.mrtnv@gmail.com>
2020-05-05 20:54:07 +0400

    28-month: Fix: Review issues

--- server/functions/src/api/possessions.ts
+++ server/functions/src/api/possessions.ts
@@ -1,13 +1,14 @@
 import * as functions from 'firebase-functions';
 import * as config from '../config';
 
+import produce from 'immer';
+
 import { Firestore } from '../core/firebase/firestore';
 import { FirestoreSelector } from '../providers/firestore_selector';
 import { PossessionStateGenerator } from '../services/possession_state_generator';
 import { GameProvider } from '../providers/game_provider';
 import { GameContextEntity } from '../models/domain/game/game_context';
 import { APIRequest } from '../core/api/request_data';
-import produce from 'immer';
 
 export const create = (firestore: Firestore, selector: FirestoreSelector) => {
   const https = functions.region(config.CLOUD_FUNCTIONS_REGION).https;
@@ -23,19 +24,19 @@ export const create = (firestore: Firestore, selector: FirestoreSelector) => {
       const possessionService = new PossessionStateGenerator();
 
       const game = await gameProvider.getGame(context.gameId);
-      const newPossisionState = possessionService.generateParticipantsPossessionState(game);
+      const newPossessionState = possessionService.generateParticipantsPossessionState(game);
 
       const updatedGame = produce(game, (draft) => {
-        draft.possessionState = newPossisionState;
+        draft.possessionState = newPossessionState;
       });
 
       await gameProvider.updateGame(updatedGame);
 
-      return newPossisionState;
+      return newPossessionState;
     };
 
-    const possisionState = updatePossessionState();
-    response.status(200).send(possisionState);
+    const possessionState = updatePossessionState();
+    response.status(200).send(possessionState);
   });
 
   return { updatePossessionsState };
