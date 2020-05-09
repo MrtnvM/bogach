4d0f7c5374929cc2da81da1c886ae7af616772e8
Artem Abramov <artem.abramov@dualbootpartners.com>
2020-05-09 20:24:52 +0300

    73-cashflow: #73 Added corner test cases

--- server/functions/src/transformers/game/participant_accounts_transformer.spec.ts
+++ server/functions/src/transformers/game/participant_accounts_transformer.spec.ts
@@ -72,6 +72,96 @@ describe('Participant Accounts Transformer Tests', () => {
 
         expect(newGameState.accounts[userId].cashFlow).toStrictEqual(-19000);
     });
+
+    test('Empty cashflow count', async () => {
+        const possessionState: PossessionState = {
+            incomes: [],
+            expenses: [],
+            assets: [],
+            liabilities: [],
+        };
+
+        const game = buildGameEntity(possessionState);
+
+        const accountsStateTransformer = new ParticipantAccountsTransformer();
+        const newGameState = accountsStateTransformer.apply(game);
+
+        expect(newGameState.accounts[userId].cashFlow).toStrictEqual(0);
+    });
+    
+    test('Int overflow cashflow count', async () => {
+        const possessionState: PossessionState = {
+            incomes: [
+                {
+                    id: 'income2',
+                    value: 1000000000000000,
+                    name: 'Карманные от бабушки',
+                    type: 'other',
+                },
+            ],
+            expenses: [
+                {
+                    id: 'expense1',
+                    name: 'Общее',
+                    value: 2000000000000000,
+                },
+            ],
+            assets: [],
+            liabilities: [],
+        };
+
+        const game = buildGameEntity(possessionState);
+
+        const accountsStateTransformer = new ParticipantAccountsTransformer();
+        const newGameState = accountsStateTransformer.apply(game);
+
+        expect(newGameState.accounts[userId].cashFlow).toStrictEqual(-1000000000000000);
+    });
+    
+    test('Empty expenses cashflow count', async () => {
+        const possessionState: PossessionState = {
+            incomes: [
+                {
+                    id: 'income2',
+                    value: 1000,
+                    name: 'Карманные от бабушки',
+                    type: 'other',
+                },
+            ],
+            expenses: [],
+            assets: [],
+            liabilities: [],
+        };
+
+        const game = buildGameEntity(possessionState);
+
+        const accountsStateTransformer = new ParticipantAccountsTransformer();
+        const newGameState = accountsStateTransformer.apply(game);
+
+        expect(newGameState.accounts[userId].cashFlow).toStrictEqual(1000);
+    });
+
+    test('Empty incomes cashflow count', async () => {
+        const possessionState: PossessionState = {
+            incomes: [],
+            expenses: [
+                {
+                    id: 'expense1',
+                    name: 'Общее',
+                    value: 1000,
+                },
+            ],
+            assets: [],
+            liabilities: [],
+        };
+
+        const game = buildGameEntity(possessionState);
+
+        const accountsStateTransformer = new ParticipantAccountsTransformer();
+        const newGameState = accountsStateTransformer.apply(game);
+
+        expect(newGameState.accounts[userId].cashFlow).toStrictEqual(-1000);
+    });
 });
 
 function buildGameEntity(possessionState: PossessionState) {
