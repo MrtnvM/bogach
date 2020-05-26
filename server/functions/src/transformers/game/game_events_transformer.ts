bd15cba21c4d841c68d13b6945414dffcf79117f
Robert <robert.badamshin@simbirsoft.com>
2020-05-26 22:08:13 +0300

    85: add server changes

--- server/functions/src/transformers/game/game_events_transformer.ts
+++ server/functions/src/transformers/game/game_events_transformer.ts
@@ -3,13 +3,13 @@ import produce from 'immer';
 import { GameTransformer } from './game_transformer';
 import { Game } from '../../models/domain/game/game';
 import { GameEvent } from '../../models/domain/game/game_event';
-import { DebenturePriceChangedEventGenerator } from '../../events/debenture/debenture_price_changed_event_generator';
-import { StockPriceChangedEventGenerator } from '../../events/stock/stock_price_changed_event_generator';
-import { IncomeEventGenerator } from '../../events/income/income_event_generator';
 import { BusinessBuyEventGenerator } from '../../events/business/buy/business_buy_event_generator';
-import { ExpenseEventGenerator } from '../../events/expense/expense_event_generator';
 import { BusinessSellEventGenerator } from '../../events/business/sell/business_sell_event_generator';
 import { BusinessSellEventProvider } from '../../services/generation/business_sell_event_provider';
+import { IncomeEventGenerator } from '../../events/income/income_event_generator';
+import { ExpenseEventGenerator } from '../../events/expense/expense_event_generator';
+import { StockPriceChangedEventGenerator } from '../../events/stock/stock_price_changed_event_generator';
+import { DebenturePriceChangedEventGenerator } from '../../events/debenture/debenture_price_changed_event_generator';
 
 export class GameEventsTransformer extends GameTransformer {
   constructor(private force: boolean = false) {
@@ -27,7 +27,7 @@ export class GameEventsTransformer extends GameTransformer {
   }
 
   private generateGameEvents(game: Game): GameEvent[] {
-    const businessSellEventGenerator = new BusinessSellEventGenerator()
+    const businessSellEventGenerator = new BusinessSellEventGenerator();
     const businessSellEventProvider = new BusinessSellEventProvider(businessSellEventGenerator);
 
     const gameEvents = [
@@ -40,7 +40,6 @@ export class GameEventsTransformer extends GameTransformer {
       BusinessBuyEventGenerator.generate(),
       BusinessBuyEventGenerator.generate(),
       ...businessSellEventProvider.generateBusinessSellEvent(game),
-      DebenturePriceChangedEventGenerator.generate(),
       IncomeEventGenerator.generate(),
       ExpenseEventGenerator.generate(),
     ];
