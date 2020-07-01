a2d68f4190236613789fa6c100bb27f348a77b0a
Robert <robert.badamshin@simbirsoft.com>
2020-07-01 15:26:33 +0300

    130: fix business sell tests

--- server/functions/src/events/business/sell/business_sell_event_handler.spec.utils.ts
+++ server/functions/src/events/business/sell/business_sell_event_handler.spec.utils.ts
@@ -1,13 +1,6 @@
 import { Game, GameEntity } from '../../../models/domain/game/game';
-import { InsuranceAsset } from '../../../models/domain/assets/insurance_asset';
-import { StockAsset } from '../../../models/domain/assets/stock_asset';
-import { RealtyAsset } from '../../../models/domain/assets/realty_asset';
 import { BusinessAsset } from '../../../models/domain/assets/business_asset';
-import { OtherAsset } from '../../../models/domain/assets/other_asset';
-import { CashAsset } from '../../../models/domain/assets/cash_asset';
-import { MortgageLiability } from '../../../models/domain/liabilities/mortgage_liability';
 import { BusinessCreditLiability } from '../../../models/domain/liabilities/business_credit';
-import { OtherLiability } from '../../../models/domain/liabilities/other_liability';
 import { UserEntity } from '../../../models/domain/user';
 import { Possessions } from '../../../models/domain/possessions';
 import { GameContext } from '../../../models/domain/game/game_context';
@@ -60,97 +53,10 @@ const business3 = create<BusinessAsset>({
 });
 
 const initialPossesssions: Possessions = {
-  incomes: [
-    {
-      id: 'income1',
-      value: 92000,
-      name: 'Зарплата',
-      type: 'salary',
-    },
-    {
-      id: 'income2',
-      value: 1000,
-      name: 'Карманные от бабушки',
-      type: 'other',
-    },
-  ],
-  expenses: [
-    {
-      id: 'expense1',
-      name: 'Общее',
-      value: 20000,
-    },
-  ],
-  assets: [
-    create<InsuranceAsset>({
-      id: 'insurance1',
-      name: 'Страховка квартиры',
-      type: 'insurance',
-      value: 50000,
-      cost: 5000,
-      duration: 12,
-      fromMonth: 1,
-      insuranceType: 'health',
-    }),
-    business1,
-    business2,
-    business3,
-    create<StockAsset>({
-      id: 'stocks1',
-      name: 'Яндекс',
-      type: 'stock',
-      averagePrice: 1000,
-      countInPortfolio: 10,
-      fairPrice: 900,
-    }),
-    create<RealtyAsset>({
-      id: 'realty1',
-      name: 'Квартира',
-      type: 'realty',
-      cost: 2000000,
-      downPayment: 1000000,
-      passiveIncomePerMonth: 14_000,
-    }),
-    create<BusinessAsset>({
-      id: 'business2',
-      name: 'Птицеферма',
-      type: 'business',
-      buyPrice: 130_000,
-      downPayment: 15_000,
-      fairPrice: 135_000,
-      passiveIncomePerMonth: 2500,
-      payback: 22,
-      sellProbability: 10,
-    }),
-    create<OtherAsset>({
-      id: 'other_asset1',
-      name: 'Биткойны',
-      type: 'other',
-      value: 30000,
-      downPayment: 30000,
-    }),
-    create<CashAsset>({
-      id: 'cash1',
-      type: 'cash',
-      value: 500,
-      name: 'Наличные',
-    }),
-  ],
+  incomes: [],
+  expenses: [],
+  assets: [business1, business2, business3],
   liabilities: [
-    create<MortgageLiability>({
-      id: 'mortgage1',
-      name: 'Ипотека',
-      type: 'mortgage',
-      value: 2_000_000,
-      monthlyPayment: 20000,
-    }),
-    create<OtherLiability>({
-      id: 'other_libility1',
-      name: 'Долг другу',
-      type: 'other',
-      value: 5000,
-      monthlyPayment: 500,
-    }),
     create<BusinessCreditLiability>({
       id: 'id1',
       value: 99_000,
