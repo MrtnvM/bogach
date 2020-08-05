44b73ece1690f7b57c018840768c418554ec4cbd
Robert <robert.badamshin@simbirsoft.com>
2020-08-05 09:28:18 +0300

    144: add stock dialog

--- client/lib/presentation/gameboard/game_events/stock/ui/stock_game_event_hooks.dart
+++ client/lib/presentation/gameboard/game_events/stock/ui/stock_game_event_hooks.dart
@@ -7,6 +7,7 @@ import 'package:cash_flow/models/domain/player_action/buy_sell_action.dart';
 import 'package:cash_flow/presentation/dialogs/dialogs.dart';
 import 'package:cash_flow/presentation/gameboard/game_events/stock/model/stock_event_data.dart';
 import 'package:cash_flow/presentation/gameboard/game_events/stock/model/stock_player_action.dart';
+import 'package:cash_flow/presentation/gameboard/widgets/dialog/game_event_info_dialog_model.dart';
 import 'package:cash_flow/resources/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -69,4 +70,22 @@ StockAsset useCurrentStock(GameEvent event) {
   );
 
   return currentStock;
+}
+
+GameEventInfoDialogModel useStockInfoDialogModel() {
+  return useMemoized(
+    () => GameEventInfoDialogModel(
+      title: Strings.stockDialogTitle,
+      description: Strings.stockDialogDescription,
+      keyPoints: {
+        Strings.stockDialogKeyPoint1:
+            Strings.stockDialogKeyPointDescription1,
+        Strings.stockDialogKeyPoint2:
+            Strings.stockDialogKeyPointDescription2,
+      },
+      riskLevel: Rating.high,
+      profitabilityLevel: Rating.high,
+      complexityLevel: Rating.high,
+    ),
+  );
 }
