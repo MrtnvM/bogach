0a66cb6bc7853803a917a185483ec4703a6bf168
Maxim <max.mrtnv@gmail.com>
2020-09-10 22:20:08 +0400

    167: develop: Configuration: Moved quest images to Firebase Storage, Changed hookah man quest description

--- server/functions/src/game_levels/levels/hookah_man/game_template.ts
+++ server/functions/src/game_levels/levels/hookah_man/game_template.ts
@@ -4,7 +4,7 @@ export const template: GameTemplate = {
   id: 'hookah_man',
   name: 'Кальянщик',
   icon:
-    'https://st2.depositphotos.com/1504872/8305/v/450/depositphotos_83059322-stock-illustration-%D0%BA%D0%B0%D0%BB%D1%8C%D1%8F%D0%BD-%D1%8D%D1%81%D0%BA%D0%B8%D0%B7-%D0%B8%D0%BB%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F.jpg',
+    'https://firebasestorage.googleapis.com/v0/b/bogach-production.appspot.com/o/quest_icons_images%2Fhookah_man.png?alt=media&token=a6979ee8-1bb0-4be3-908a-558dea33f18d',
   possessions: {
     incomes: [
       {
