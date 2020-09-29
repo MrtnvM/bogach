1ea1929b119af00f6e1fe99e5f84fec1ae141319
Maxim Martynov <max.mrtnv@gmail.com>
2020-09-29 18:53:50 +0400

    Fix: Logout setting

--- client/lib/widgets/settings/logout_setting.dart
+++ client/lib/widgets/settings/logout_setting.dart
@@ -2,6 +2,7 @@ import 'package:cash_flow/core/hooks/dispatcher.dart';
 import 'package:cash_flow/features/profile/actions/logout_action.dart';
 import 'package:cash_flow/navigation/app_router.dart';
 import 'package:cash_flow/presentation/login/login_page.dart';
+import 'package:dash_kit_core/dash_kit_core.dart';
 import 'package:flutter/material.dart';
 import 'package:dash_kit_control_panel/dash_kit_control_panel.dart';
 
@@ -21,19 +22,11 @@ class LogoutSetting extends StatefulWidget implements ControlPanelSetting {
 class _LogoutSettingState extends State<LogoutSetting> {
   @override
   Widget build(BuildContext context) {
-    final dispatch = useDispatcher();
-
-    final logout = () {
-      dispatch(LogoutAction())
-          .then((_) => _onLogoutFinished())
-          .catchError((_) => _onLogoutFinished());
-    };
-
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       child: RaisedButton(
         color: Colors.green.withAlpha(240),
-        onPressed: logout,
+        onPressed: _logout,
         child: FittedBox(
           child: Text(
             'Log Out',
@@ -46,6 +39,12 @@ class _LogoutSettingState extends State<LogoutSetting> {
         ),
       ),
     );
+  }
+
+  void _logout() {
+    StoreProvider.dispatchFuture(context, LogoutAction())
+        .then((_) => _onLogoutFinished())
+        .catchError((_) => _onLogoutFinished());
   }
 
   void _onLogoutFinished() {
