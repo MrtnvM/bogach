859c5f48a2d8b69cece39f7a76c35fd9aef09672
Maxim Martynov <max.mrtnv@gmail.com>
2020-10-11 19:12:23 +0400

    Fix: Creation of singleplayer game

--- client/lib/utils/core/date.dart
+++ client/lib/utils/core/date.dart
@@ -1,2 +1,9 @@
-DateTime fromISO8601DateJson(String date) => DateTime.parse(date);
+DateTime fromISO8601DateJson(String date) {
+  if (date == null) {
+    return null;
+  }
+
+  return DateTime.parse(date);
+}
+
 String toISO8601DateJson(DateTime date) => date.toIso8601String();
