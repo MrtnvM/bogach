66b6c167654ef4e764b7a45aab9901a2313f035f
Maxim Martynov <max.mrtnv@gmail.com>
2020-10-27 01:33:02 +0400

    UI: Default tab is Actions Tab

--- client/lib/presentation/gameboard/gameboard.dart
+++ client/lib/presentation/gameboard/gameboard.dart
@@ -4,19 +4,19 @@ import 'package:cash_flow/models/domain/game/game/type/game_type.dart';
 import 'package:cash_flow/presentation/gameboard/tabs/actions_tab.dart';
 import 'package:cash_flow/presentation/gameboard/tabs/finances_tab.dart';
 import 'package:cash_flow/presentation/gameboard/tabs/progress_tab.dart';
-import 'package:cash_flow/presentation/gameboard/winners_page.dart';
-import 'package:cash_flow/resources/strings.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:cash_flow/presentation/gameboard/widgets/bars/bottom_bar.dart';
+import 'package:cash_flow/presentation/gameboard/winners_page.dart';
 import 'package:cash_flow/resources/colors.dart';
 import 'package:cash_flow/resources/images.dart';
+import 'package:cash_flow/resources/strings.dart';
 import 'package:dash_kit_loadable/dash_kit_loadable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class GameBoard extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final selectedIndex = useState(0);
+    final selectedIndex = useState(1);
     final activeGameState = useGlobalState((s) => s.game.activeGameState);
     final gameExists = useCurrentGame((g) => g != null);
     final isMultiplayer = useCurrentGame(
