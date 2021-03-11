df631b3e9937ee472d8c27be43b49813e3b72bca
Maxim Martynov <max.mrtnv@gmail.com>
2021-03-12 03:56:30 +0400

    Implementation: Room page redesign

--- client/lib/models/domain/game/game_template/game_template.dart
+++ client/lib/models/domain/game/game_template/game_template.dart
@@ -1,6 +1,8 @@
+import 'package:cash_flow/resources/strings.dart';
 import 'package:dash_kit_core/dash_kit_core.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:cash_flow/utils/extensions/extensions.dart';
 
 import '../account/account.dart';
 import '../target/target.dart';
@@ -26,4 +28,13 @@ class GameTemplate implements StoreListItem {
   final Account accountState;
   final Target target;
   final String image;
+
+  String getDescription() {
+    final targetTitle = mapTargetTypeToString(target.type).toLowerCase();
+    final targetValue = target.value.round().toPrice();
+    final description = '${Strings.reach} $targetTitle '
+        '${Strings.wordIn} $targetValue';
+
+    return description;
+  }
 }
--- client/lib/presentation/multiplayer/room_page.dart
+++ client/lib/presentation/multiplayer/room_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cash_flow/analytics/sender/common/analytics_sender.dart';
 import 'package:cash_flow/analytics/sender/common/session_tracker.dart';
 import 'package:cash_flow/app/operation.dart';
@@ -12,23 +15,26 @@ import 'package:cash_flow/features/multiplayer/actions/room_listening_actions.da
 import 'package:cash_flow/features/multiplayer/actions/set_room_participant_ready_action.dart';
 import 'package:cash_flow/features/multiplayer/actions/share_room_invite_link_action.dart';
 import 'package:cash_flow/models/domain/game/game_context/game_context.dart';
+import 'package:cash_flow/models/domain/game/game_template/game_template.dart';
+import 'package:cash_flow/models/domain/room/room.dart';
 import 'package:cash_flow/models/domain/room/room_participant.dart';
 import 'package:cash_flow/models/domain/user/user_profile.dart';
 import 'package:cash_flow/navigation/app_router.dart';
 import 'package:cash_flow/presentation/dialogs/dialogs.dart';
 import 'package:cash_flow/presentation/gameboard/gameboard.dart';
-import 'package:cash_flow/presentation/multiplayer/widgets/user_profile_item.dart';
+import 'package:cash_flow/presentation/main/widgets/game_type_title.dart';
 import 'package:cash_flow/presentation/tutorial/tutorial_page.dart';
 import 'package:cash_flow/resources/colors.dart';
 import 'package:cash_flow/resources/strings.dart';
 import 'package:cash_flow/resources/styles.dart';
 import 'package:cash_flow/utils/core/tuple.dart';
-import 'package:cash_flow/widgets/containers/cash_flow_scaffold.dart';
-import 'package:cash_flow/widgets/containers/note.dart';
+import 'package:cash_flow/widgets/avatar/avatar_widget.dart';
 import 'package:dash_kit_core/dash_kit_core.dart';
 import 'package:dash_kit_loadable/dash_kit_loadable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:shimmer/shimmer.dart';
 
 class RoomPage extends HookWidget {
   @override
@@ -53,45 +59,174 @@ class RoomPage extends HookWidget {
 
     final mediaQueryData = useAdaptiveMediaQueryData();
 
-    return LoadableView(
-      backgroundColor: ColorRes.black80,
-      isLoading: isActionInProgress,
-      child: CashFlowScaffold(
-        title: Strings.waitingPlayers,
-        showBackArrow: true,
-        horizontalPadding: 16,
-        child: MediaQuery(
-          data: mediaQueryData,
-          child: SizedBox(
-            height: 300,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Expanded(child: _ParticipantListWidget()),
-                const Divider(height: 1),
-                const SizedBox(height: 16),
-                if (isCurrentUserRoomOwner)
-                  _InviteFriendsNote()
-                else
-                  _JoinToRoomNote(),
-                const SizedBox(height: 24),
-                if (isCurrentUserRoomOwner) ...[
-                  Row(
-                    children: const [
-                      Expanded(child: _InviteButton()),
-                      SizedBox(height: 16, width: 16),
-                      Expanded(child: _StartGameButton()),
-                    ],
-                  ),
-                  const SizedBox(height: 24),
-                ] else if (!isParticipantAlreadyJoined) ...[
-                  const _JoinRoomButton(),
-                  const SizedBox(height: 24),
-                ],
-              ],
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle.light,
+      child: LoadableView(
+        backgroundColor: ColorRes.black80,
+        isLoading: isActionInProgress,
+        child: Scaffold(
+          backgroundColor: ColorRes.mainPageBackground,
+          body: MediaQuery(
+            data: mediaQueryData,
+            child: SafeArea(
+              top: false,
+              child: _buildBody(
+                context: context,
+                isCurrentUserRoomOwner: isCurrentUserRoomOwner,
+                isParticipantAlreadyJoined: isParticipantAlreadyJoined,
+                room: room,
+              ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Column _buildBody({
+    BuildContext context,
+    bool isCurrentUserRoomOwner,
+    bool isParticipantAlreadyJoined,
+    Room room,
+  }) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _Header(templateId: room?.gameTemplateId),
+        const SizedBox(height: 16),
+        GameTypeTitle(text: Strings.players),
+        const SizedBox(height: 16),
+        const Divider(height: 1, indent: 16, endIndent: 16),
+        Expanded(child: _ParticipantListWidget()),
+        const Divider(height: 1, indent: 16, endIndent: 16),
+        const SizedBox(height: 24),
+        if (isCurrentUserRoomOwner) ...[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: const [
+                Expanded(child: _InviteButton()),
+                SizedBox(height: 16, width: 16),
+                Expanded(child: _StartGameButton()),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+        ] else if (!isParticipantAlreadyJoined) ...[
+          const _JoinRoomButton(),
+          const SizedBox(height: 16),
+        ],
+      ],
+    );
+  }
+}
+
+class _Header extends HookWidget {
+  const _Header({Key key, @required this.templateId}) : super(key: key);
+
+  final String templateId;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = useScreenSize();
+    final headerDesiredHeight = screenSize.height * 0.3;
+    final headerHeight =
+        min(headerDesiredHeight, screenSize.height > 700 ? 300 : 200);
+
+    final template = useGlobalState(
+      (s) => s.newGame.gameTemplates.itemsMap[templateId],
+    );
+
+    return SizedBox(
+      width: double.infinity,
+      height: headerHeight,
+      child: Stack(
+        children: [
+          if (template == null)
+            _buildShimmer()
+          else
+            _buildHeaderImage(template),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            right: 16,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (template == null)
+                  _buildPlaceholder(screenSize.width * 0.4)
+                else
+                  Text(
+                    template.name,
+                    style: Styles.bodyWhiteBold.copyWith(fontSize: 16),
+                  ),
+                const SizedBox(height: 4),
+                if (template == null)
+                  _buildPlaceholder(screenSize.width * 0.7)
+                else
+                  Text(
+                    template.getDescription(),
+                    style: Styles.body1.copyWith(fontSize: 16),
+                  ),
+              ],
+            ),
+          ),
+          Positioned(
+            child: GestureDetector(
+              onTap: appRouter.goBack,
+              behavior: HitTestBehavior.translucent,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16 + MediaQuery.of(context).padding.top,
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white.withAlpha(200),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildPlaceholder(double width) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.withAlpha(140),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      height: 20,
+      width: width,
+    );
+  }
+
+  Stack _buildHeaderImage(GameTemplate template) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image(
+            image: CachedNetworkImageProvider(template.image),
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned.fill(
+          child: Container(color: Colors.black.withAlpha(100)),
+        ),
+      ],
+    );
+  }
+
+  Shimmer _buildShimmer() {
+    return Shimmer.fromColors(
+      baseColor: const Color(0xFFDDDDDD),
+      highlightColor: const Color(0xFFFFFFFF),
+      child: Container(
+        constraints: const BoxConstraints.expand(),
+        color: Colors.grey,
       ),
     );
   }
@@ -229,20 +364,6 @@ class _Button extends StatelessWidget {
   }
 }
 
-class _InviteFriendsNote extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Note(title: Strings.inviteFriendsToStart);
-  }
-}
-
-class _JoinToRoomNote extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Note(title: Strings.joinToRoom);
-  }
-}
-
 void _useAutoTransitionToCreatedGame() {
   final userId = useUserId();
   final room = useGlobalState((s) => s.multiplayer.currentRoom);
@@ -270,7 +391,11 @@ void _useAutoTransitionToCreatedGame() {
       });
     }
 
-    return null;
+    return () {
+      if (room?.id != null) {
+        dispatch(StopListeningRoomUpdatesAction(room.id));
+      }
+    };
   }, [room?.gameId]);
 }
 
@@ -290,28 +415,18 @@ class _ParticipantListWidget extends HookWidget {
         .map((p) => Tuple(p, userProfiles.itemsMap[p.id]))
         .toList();
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.only(top: 8),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(
-          minWidth: double.infinity,
-          maxWidth: double.infinity,
-        ),
-        child: Wrap(
-          direction: Axis.horizontal,
-          alignment: WrapAlignment.center,
-          children: <Widget>[
-            for (final participant in participantsList)
-              _buildParticipantWidget(
-                participant: participant.item1,
-                profile: participant.item2 ??
-                    UserProfile.unknownUser(
-                      participant.item1.id,
-                    ),
-              )
-          ],
-        ),
-      ),
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      children: [
+        for (final participant in participantsList)
+          _buildParticipantWidget(
+            participant: participant.item1,
+            profile: participant.item2 ??
+                UserProfile.unknownUser(
+                  participant.item1.id,
+                ),
+          )
+      ],
     );
   }
 
@@ -321,47 +436,36 @@ class _ParticipantListWidget extends HookWidget {
   }) {
     IconData icon;
     String status;
+    Color color;
 
     switch (participant.status) {
       case RoomParticipantStatus.waiting:
         status = Strings.waiting;
         icon = Icons.access_time;
+        color = ColorRes.grey;
         break;
 
       case RoomParticipantStatus.ready:
         status = Strings.readyForGame;
         icon = Icons.check;
+
+        color = ColorRes.mainGreen;
         break;
     }
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-      decoration: BoxDecoration(
-        color: ColorRes.lightGreen.withAlpha(40),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      margin: const EdgeInsets.all(8),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            height: 94,
-            child: UserProfileItem(profile),
-          ),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Icon(
-                icon,
-                color: ColorRes.white64,
-                size: 14,
-              ),
-              const SizedBox(width: 4),
-              Text(status, style: Styles.body1.copyWith(fontSize: 11)),
-            ],
-          )
+    return ListTile(
+      title: Text(profile.fullName, style: Styles.bodyBlackSemibold),
+      subtitle: Row(
+        children: [
+          Icon(icon, size: 14, color: color),
+          const SizedBox(width: 4),
+          Expanded(child: Text(status)),
         ],
+      ),
+      leading: UserAvatar(
+        url: profile.avatarUrl,
+        size: 52,
+        borderColor: color,
       ),
     );
   }
