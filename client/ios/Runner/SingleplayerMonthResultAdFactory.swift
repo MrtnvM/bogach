920c15980e2f6777afddeb5fc255ee175c864c5a
Maxim Martynov <max.mrtnv@gmail.com>
2021-04-30 04:46:11 +0400

    Refactoring: Removed unilinks dependency

---
+++ client/ios/Runner/SingleplayerMonthResultAdFactory.swift
@@ -0,0 +1,9 @@
+//
+//  SingleplayerMonthResultAdFactory.swift
+//  Runner
+//
+//  Created by Максим Мартынов on 30.04.2021.
+//  Copyright © 2021 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
