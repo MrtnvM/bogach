9ed33a0c0189a760e6b5342a8124916c834b1d8c
Maxim Martynov <max.mrtnv@gmail.com>
2021-05-04 16:01:27 +0400

    Configuration: Android Singleplayer month result ad ID

--- client/lib/features/advertising/google_ad_units_ids.dart
+++ client/lib/features/advertising/google_ad_units_ids.dart
@@ -5,14 +5,20 @@ enum AdMobAdUnit {
   test,
 }
 
-// TODO this is test ids, need to add release if release build
-// https://gitlab.com/cash-flow-team/cash-flow/-/issues/304
-// release ids stored in google ad mob account
 String getNativeGoogleAdUnitId([AdMobAdUnit adUnit = AdMobAdUnit.test]) {
   assert(adUnit != null);
 
   if (Platform.isAndroid) {
-    return 'ca-app-pub-3940256099942544/2247696110';
+    switch (adUnit) {
+      case AdMobAdUnit.singleplayerMonthResult:
+        return 'ca-app-pub-3584659164590042/4050740041';
+
+      case AdMobAdUnit.test:
+        return 'ca-app-pub-3940256099942544/2247696110';
+
+      default:
+        throw UnsupportedError('Unsupported iOS ad unit');
+    }
   }
 
   if (Platform.isIOS) {
