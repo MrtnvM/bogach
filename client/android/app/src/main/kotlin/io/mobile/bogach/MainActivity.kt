b68b7039f85e0400ba5aeda22216b4140b0a0487
Maxim Martynov <max.mrtnv@gmail.com>
2021-05-04 15:53:16 +0400

    Implementation: Google Ads for singleplayer month result on Android

--- client/android/app/src/main/kotlin/io/mobile/bogach/MainActivity.kt
+++ client/android/app/src/main/kotlin/io/mobile/bogach/MainActivity.kt
@@ -4,23 +4,24 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
-import io.mobile.bogach.goggle_ad.GoogleAdConstants
-import io.mobile.bogach.goggle_ad.ListTileNativeAdFactory
+import io.mobile.bogach.google_ads.GoogleAdConstants
+import io.mobile.bogach.google_ads.SingleplayerMonthResultAdFactory
 
 class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
 
-        //TODO change factoryId https://gitlab.com/cash-flow-team/cash-flow/-/issues/304
         GoogleMobileAdsPlugin.registerNativeAdFactory(
-            flutterEngine,
-            GoogleAdConstants.factoryId,
-            ListTileNativeAdFactory(context)
+                flutterEngine,
+                GoogleAdConstants.singleplayerMonthResultAd,
+                SingleplayerMonthResultAdFactory(context)
         )
     }
 
     override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
-        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, GoogleAdConstants.factoryId)
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(
+                flutterEngine, GoogleAdConstants.singleplayerMonthResultAd
+        )
     }
 }
--- client/android/app/src/main/kotlin/io/mobile/bogach/goggle_ad/GoogleAdConstants.kt
+++
@@ -1,6 +0,0 @@
-package io.mobile.bogach.goggle_ad
-
-object GoogleAdConstants {
-
-    const val factoryId = "listTile"
-}
