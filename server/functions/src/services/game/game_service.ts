2c153ab76ad2a568b082d4d743a07c4443dab2c7
Maxim Martynov <max.mrtnv@gmail.com>
2021-06-14 23:19:46 +0400

    Fix: Creating statistic entities for quests

--- server/functions/src/services/game/game_service.ts
+++ server/functions/src/services/game/game_service.ts
@@ -162,10 +162,14 @@ export class GameService {
       }
 
       if (updatedGame.state.gameStatus === 'game_over') {
-        const statistic = await this.gameProvider.updateLevelStatistic(updatedGame);
-        updatedGame = applyGameTransformers(updatedGame, [
-          new StatisticsTransformer(statistic, userId),
-        ]);
+        const isSingleplayerGame = game.type === 'singleplayer' && !game.config.level;
+
+        if (isSingleplayerGame) {
+          const statistic = await this.gameProvider.updateLevelStatistic(updatedGame);
+          updatedGame = applyGameTransformers(updatedGame, [
+            new StatisticsTransformer(statistic, userId),
+          ]);
+        }
 
         await this.gameProvider.updateGameParticipantsCompletedGames(updatedGame);
       }
