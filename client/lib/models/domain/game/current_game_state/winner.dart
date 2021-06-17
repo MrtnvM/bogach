7bba081a6eb22adc98785b1012b1f4e19ea8d18d
Maxim Martynov <max.mrtnv@gmail.com>
2021-06-17 17:16:51 +0400

    Fix: Parsing Winner object

--- client/lib/models/domain/game/current_game_state/winner.dart
+++ client/lib/models/domain/game/current_game_state/winner.dart
@@ -8,6 +8,7 @@ class Winner with _$Winner {
   factory Winner({
     required String userId,
     required double targetValue,
+    @JsonKey(defaultValue: 0) required int benchmark,
   }) = _Winner;
 
   factory Winner.fromJson(Map<String, dynamic> json) => _$WinnerFromJson(json);
