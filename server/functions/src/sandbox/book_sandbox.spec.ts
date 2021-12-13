0939f1a5565a6454f76235f5c271a904553f8bdb
Maxim Martynov <max.mrtnv@gmail.com>
2021-12-14 00:38:19 +0400

    Book sandbox updates

--- server/functions/src/sandbox/book_sandbox.spec.ts
+++ server/functions/src/sandbox/book_sandbox.spec.ts
@@ -9,28 +9,38 @@ import { BookRecommendationsService } from '../services/recommendations/book_rec
 import { FirestoreBookRecommendationsDAO } from '../dao/firestore/firestore_book_recommendations_dao';
 import { FIREBASE_STAGING_CONFIG, LITRES_PARTNER_ID, RECOMMENDATION_BOOKS_IDS } from '../config';
 import { LitresDataProvider } from '../providers/recommendations/litres_data_provider';
+import { RecommendationBook } from '../models/domain/recommendations/books/recommendation_book';
 
 admin.initializeApp(FIREBASE_STAGING_CONFIG);
 
 describe('Sandbox', () => {
-  test('Parse Litres books', async () => {
-    jest.setTimeout(150_000);
+  jest.setTimeout(150_000);
 
-    const firestore = admin.firestore();
-    const selector = new FirestoreSelector(firestore);
-    const firestoreInstance = new Firestore();
+  const firestore = admin.firestore();
+  const selector = new FirestoreSelector(firestore);
+  const firestoreInstance = new Firestore();
 
-    const slackProvider = new SlackProvider();
-    const litresDataProvider = new LitresDataProvider(LITRES_PARTNER_ID);
+  const slackProvider = new SlackProvider();
+  const litresDataProvider = new LitresDataProvider(LITRES_PARTNER_ID);
 
-    const bookDao = new FirestoreBookRecommendationsDAO(selector, firestoreInstance);
-    const bookProvider = new BookRecommendationsProvider(bookDao);
-    const bookService = new BookRecommendationsService(
-      litresDataProvider,
-      slackProvider,
-      bookProvider
-    );
+  const bookDao = new FirestoreBookRecommendationsDAO(selector, firestoreInstance);
+  const bookProvider = new BookRecommendationsProvider(bookDao);
+  const bookService = new BookRecommendationsService(
+    litresDataProvider,
+    slackProvider,
+    bookProvider
+  );
 
+  test.skip('Parse Litres books', async () => {
     await bookService.updateBooksData(RECOMMENDATION_BOOKS_IDS);
+  });
+
+  test.skip('Update book data', async () => {
+    const book: Partial<RecommendationBook> = {
+      bookId: undefined,
+    };
+
+    const result = await bookService.updateBook(book);
+    console.log('RESULT: ', result);
   });
 });
