d71e11f0bf90783c320129850ace8aaa6d270b23
Maxim Martynov <max.mrtnv@gmail.com>
2021-12-17 21:15:31 +0400

    Configuration: Simplyfied disabling sending online status

--- client/lib/presentation/main/main_page.dart
+++ client/lib/presentation/main/main_page.dart
@@ -24,6 +24,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'main_page_tab_provider.dart';
 
+var _isOnlineStatusEnabled = true;
+
 class MainPage extends HookWidget {
   const MainPage();
 
@@ -33,13 +35,15 @@ class MainPage extends HookWidget {
     final dispatch = useDispatcher();
 
     final setOnline = () {
-      dispatch(SetUserOnlineAction(
-        user: OnlineProfile(
-          userId: user.id,
-          avatarUrl: user.avatarUrl ?? Images.defaultAvatarUrl,
-          fullName: user.fullName,
-        ),
-      ));
+      if (_isOnlineStatusEnabled) {
+        dispatch(SetUserOnlineAction(
+          user: OnlineProfile(
+            userId: user.id,
+            avatarUrl: user.avatarUrl ?? Images.defaultAvatarUrl,
+            fullName: user.fullName,
+          ),
+        ));
+      }
     };
 
     useUserIdSender();
